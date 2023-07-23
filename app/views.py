5ebe4d5477af886c90ef89925f7089faadf27d75
Joshua Caponigro <joshcap@email.unc.edu>
2023-07-23 15:34:51 -0500

    some async impl

--- app/views.py
+++ app/views.py
@@ -2,31 +2,32 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.db.models import Q
 from app.models import Movie
 from django.http import HttpResponseNotFound, HttpResponseRedirect, JsonResponse
+from asgiref.sync import sync_to_async
 
-def stream(request, query):
+async def stream(request, query):
   try:
-    movie = Movie.objects.get(id=query)
+    movie = await sync_to_async(Movie.objects.get(id=query))
   except Movie.DoesNotExist:
     return HttpResponseNotFound("Movie not found")
   return movie.stream_external_video_adv(request)
     
-def index(request):
+async def index(request):
   query = request.GET.get('q', '')
   type = request.GET.get('type', 'all')
   source = request.GET.get('source', 'all')
   media = request.GET.get('media', 'all')
   
   if query:
-    movies = Movie.objects.filter(Q(title__icontains=query) | Q(path__icontains=query))
+    movies = await sync_to_async(Movie.objects.filter(Q(title__icontains=query) | Q(path__icontains=query)))
   else:
-    movies = Movie.objects.all()
+    movies = await sync_to_async(Movie.objects.all())
 
   if type != "all":
-    movies = movies.filter(type=type)
+    movies = await sync_to_async(movies.filter(type=type))
   if source != "all":
-    movies = movies.filter(source=source)
+    movies = await sync_to_async(movies.filter(source=source))
   if media != "all":
-    movies = movies.filter(path__icontains=media)
+    movies = await sync_to_async(movies.filter(path__icontains=media))
 
   return render(request, 'index.html', {'movies': movies})
 
@@ -34,14 +35,14 @@ def index(request):
 #                 TV APIs                   #
 #############################################
 
-def get_link(request):
+async def get_link(request):
     # If query is not a number, look up title
     query = request.GET.get('q', '')
 
     if query.isdigit():
-        movie = get_object_or_404(Movie, id=query)
+        movie = await sync_to_async(get_object_or_404(Movie, id=query))
     else:
-        movie = Movie.objects.filter(title__icontains=query).first()
+        movie = await sync_to_async(Movie.objects.filter(title__icontains=query).first())
     
     if movie:
         # movie.link is a URL to the video file
@@ -49,11 +50,11 @@ def get_link(request):
     else:
         return HttpResponseNotFound("No movie found")
 
-def get_results(request):
+async def get_results(request):
     # If query is not a number, look up title
     query = request.GET.get('q', '')
 
-    movie = Movie.objects.filter(title__icontains=query).only('id', 'title', 'link')
+    movie = await sync_to_async(Movie.objects.filter(title__icontains=query).only('id', 'title', 'link'))
     
     if movie:
       # movie.link is a URL to the video file

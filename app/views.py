4fb1e11b310bcd772908ddf623b70f7ab0284ee3
Joshua Caponigro <joshcap@email.unc.edu>
2023-07-22 21:54:52 -0500

    Query simplification

--- app/views.py
+++ app/views.py
@@ -4,19 +4,25 @@ from app.models import Movie
 
 def stream(request, query):
   movie = Movie.objects.get(id=query)
+  # if movie.type == "mkv":
+  #   return stream_mkv(request, query)
   return movie.stream_external_video()
+
+def stream_mkv(request, query):
+  movie = Movie.objects.get(id=query)
+  return render(request, 'mkv.html', {'movie': movie})
 
 def index(request):
   query = request.GET.get('q', '')
   type = request.GET.get('type', 'all')
+  source = request.GET.get('source', 'all')
+  
   if query:
-      if type == "all":
-        movies = Movie.objects.filter(Q(title__icontains=query) | Q(path__icontains=query))
-      else:
-        movies = Movie.objects.filter(Q(title__icontains=query) | Q(path__icontains=query)).filter(type=type)
+    movies = Movie.objects.filter(Q(title__icontains=query) | Q(path__icontains=query))
   else:
-      if type == "all":
-        movies = Movie.objects.all()
-      else:
-        movies = Movie.objects.filter(type=type)
+    movies = Movie.objects.all()
+  if type != "all":
+    movies = movies.filter(type=type)
+  if source != "all":
+    movies = movies.filter(source=source)
   return render(request, 'index.html', {'movies': movies})

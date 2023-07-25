cc319e555b4deed68407e95503f385e6ac18cddd
Joshua Caponigro <joshcap@email.unc.edu>
2023-07-25 01:11:27 -0500

    Render results via JS, images, pagination

--- app/website_views.py
+++ app/website_views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import HttpResponseNotFound, JsonResponse
 from django.shortcuts import render
@@ -16,21 +17,20 @@ class IndexView(View):
 
 
 class SearchAPI(View):
+    MOVIES_PER_PAGE: int = 200
+
     def get(self, request):
-        query = request.GET.get('q', '')
+        query = request.GET.get('q', 'all')
         type = request.GET.get('type', 'all')
         source = request.GET.get('source', 'all')
         media = request.GET.get('media', 'all')
 
-        if query:
+        if query != "all":
             movies = Movie.objects.filter(
                 Q(title__icontains=query) | Q(path__icontains=query))
         else:
-            if type != "all" or source != "all" or media != "all":
-                movies = Movie.objects.all()
-            else:
-                movies = Movie.objects.filter(title__icontains="family guy")
-
+            movies = Movie.objects.all()
+            
         if type != "all":
             movies = movies.filter(type=type)
         if source != "all":
@@ -38,7 +38,14 @@ class SearchAPI(View):
         if media != "all":
             movies = movies.filter(path__icontains=media)
 
-        return JsonResponse([mov.json() for mov in movies], safe=False)
+        paginator = Paginator(movies, self.MOVIES_PER_PAGE) 
+
+        page_number = request.GET.get('page', 1)
+        page_obj = paginator.get_page(page_number)
+
+        movies = list(page_obj.object_list.values('id', 'title', 'link', 'source', 'type', 'path'))
+
+        return JsonResponse(movies, safe=False)
 
 
 def stream(request, query):

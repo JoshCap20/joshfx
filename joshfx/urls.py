a7f57809da25a324de75cb9f4064a3b584c72f57
Joshua Caponigro <joshcap@email.unc.edu>
2023-07-30 15:07:33 -0500

    static file settings

--- joshfx/urls.py
+++ joshfx/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -18,4 +20,4 @@ urlpatterns = [
     ## TV APIs
     path("api/", get_link, name="get_link"),
     path("results/", get_results, name="get_results"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
